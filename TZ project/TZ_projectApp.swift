6ce06c68eb9f8ab58e70f2fb83a4b332ab998e4d
ArnurDev <mamyrovarnur99@gmail.com>
2025-06-01 11:20:57 +0500

    Initial Commit

---
+++ TZ project/TZ_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TZ_projectApp.swift
+//  TZ project
+//
+//  Created by Гаухар Байкен on 01.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TZ_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TZ projectTests/TZ_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  TZ_projectTests.swift
+//  TZ projectTests
+//
+//  Created by Гаухар Байкен on 01.06.2025.
+//
+
+import Testing
+@testable import TZ_project
+
+struct TZ_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
